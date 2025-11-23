f5fa4fa7accd815dcb9de2d93378f3c758a71830
Brian Reed <iambriansreed@gmail.com>
2025-11-23 00:15:21 -0500

    add hcaptcha

--- src/apps/api.iambrian.ts
+++ src/apps/api.iambrian.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import { verify } from 'hcaptcha';
+
 import getSheet from '../utils/googleSheets';
 import { spotifySearch } from '../utils/spotify';
 
 const app = express();
+
+const HCAPTCHA_SECRET = process.env.HCAPTCHA_SECRET || '';
 
 app.get('/', function (_req, res) {
     res.send('iambrian beffe');
@@ -32,27 +36,35 @@ async function addRow(body: { email: string; message: string; type: string }) {
 }
 
 app.post('/contact', async function (req, res) {
-    if (req.body.topyenoh === true)
-        await addRow({
-            ...req.body,
-            type: 'contact',
-        });
+    const { token } = req.body;
 
-    res.json({ success: true });
+    verify(HCAPTCHA_SECRET, token).then(async (data) => {
+        if (data.success === true) {
+            await addRow({
+                ...req.body,
+                type: 'contact',
+            });
+        }
+
+        res.json({ success: data.success });
+    });
 });
 
 app.post('/quiz', async function (req, res) {
-    const { message } = req.body;
+    const { message, token } = req.body;
     const data = typeof message === 'string' ? JSON.parse(message) : message;
 
-    if (req.body.topyenoh === true)
-        await addRow({
-            ...req.body,
-            message: JSON.stringify(data, null, 4),
-            type: 'quiz',
-        });
+    verify(HCAPTCHA_SECRET, token).then(async (data) => {
+        if (data.success === true) {
+            await addRow({
+                ...req.body,
+                message: JSON.stringify(data, null, 4),
+                type: 'quiz',
+            });
+        }
+    });
 
-    res.json({ success: true });
+    res.json({ success: data.success });
 });
 
 export default app;
